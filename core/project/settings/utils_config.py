1cd4ee1464172c53f346aa9b94b552aa0d8963d0
edmundrotimi <edmundrotimi@gmail.com>
2025-01-03 11:45:02 +0200

    register custom django filters

--- core/project/settings/utils_config.py
+++ core/project/settings/utils_config.py
@@ -40,6 +40,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'libraries': {
+                'custom_filters': 'core.project.settings.django.filters',
+            },
         },
     },
 ]
