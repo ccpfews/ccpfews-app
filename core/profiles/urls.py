4a262c3938f1512fb2492c680bb9ee99e1d6d37d
edmundrotimi <edmundrotimi@gmail.com>
2025-01-03 15:36:04 +0200

    update url paths

--- core/profiles/urls.py
+++ core/profiles/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import Team, TeamDetails  # type: ignore
 
 urlpatterns = [
-    path('', Team.as_view(), name='team_listings'),
     path('<slug:slug>/details/', TeamDetails.as_view(), name='team_details'),
+    path('', Team.as_view(), name='team_listings'),
 ]
