56bce743c22ef94d272cfb10298b60349ac30a5d
edmundrotimi <edmundrotimi@gmail.com>
2025-01-03 11:10:34 +0200

    add simple history

--- core/events/models/abstracts.py
+++ core/events/models/abstracts.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from simple_history.models import HistoricalRecords
 
 from core.profiles.utils.validate_image import clean_image
 
@@ -36,6 +37,21 @@ class Presentation(models.Model):
         blank=True,
         help_text='additional event images'
     )
+    gallery_image_5 = models.ImageField(
+        upload_to='events/images',
+        validators=[clean_image],
+        null=True,
+        blank=True,
+        help_text='additional event images'
+    )
+    gallery_image_6 = models.ImageField(
+        upload_to='events/images',
+        validators=[clean_image],
+        null=True,
+        blank=True,
+        help_text='additional event images'
+    )
+    history = HistoricalRecords(inherit=True)
     publish = models.BooleanField(default=True)
     post_scheduling = models.DateTimeField(
         default=timezone.now,
@@ -43,8 +59,10 @@ class Presentation(models.Model):
         help_text='post now or set date and time to post last later;\
                                                 post publish has to be ticked for this to work',
     )
-    date_published = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateTimeField(default=timezone.now, verbose_name='Date/Time', help_text='dated last updated')
+    date_published = models.DateTimeField(default=timezone.now)
+    date_updated = models.DateTimeField(
+        default=timezone.now, verbose_name='Date/Time Updated', help_text='dated last updated'
+    )
 
     class Meta:
         abstract = True
